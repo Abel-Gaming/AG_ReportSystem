cafa6fe1d969bb150994fd70b3716644c602b633
Abel Gaming <lcpdpolice4939@gmail.com>
2023-04-11 17:00:53 -0400

    Update config.lua

--- config.lua
+++ config.lua
@@ -2,7 +2,7 @@ Config = {}
 
 -- Discord
 Config.EnableDiscordNotifications = true
-Config.NotificationWebhook = 'https://discordapp.com/api/webhooks/931532800321011773/KyF30f_IPaHwavtkyGh8FQHfKFRIH6SPqZ8i-t8IcL9fM-c_XiEUqAydLmgOa6VflNTz'
+Config.NotificationWebhook = ''
 
 Config.UseTaggedRole = true
 Config.TaggedRoleID = '777291535623520276'
